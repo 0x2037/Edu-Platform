a3490cab84a8c0d704fa1b562cca99871f7bd8bb
Dănilă Daniel <daniladanielmarian@gmail.com>
2021-08-19 16:52:12 +0300

    Arhitectura back-end-ului

---
+++ back-end/src/test/java/com/x2037/eduplatform/EduPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x2037.eduplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
